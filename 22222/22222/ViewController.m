404ddd6749a33f16e1f6e2a0500dd104dfd31a74
wangfei521 <785904026@qq.com>
2016-06-17 14:58:42 +0800

    提交工程

---
+++ 22222/22222/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  22222
+//
+//  Created by ma c on 16/6/17.
+//  Copyright © 2016年 wangfei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"aeta");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
